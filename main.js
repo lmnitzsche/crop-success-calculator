fd4d69fca90976f2dce45bf8b88155dbfa2a77f1
Caleb Sutton <115527079+C4LSUTT@users.noreply.github.com>
2023-09-09 10:34:27 -0500

    added generalplan comments and a switch statement to be recycled

---
+++ main.js
@@ -0,0 +1,28 @@
+//Calculate probability
+
+
+//Soil States
+//calculation to make state
+
+
+
+2
+
+
+
+
+function setSoilState(state){
+switch(state){
+    case x:
+        //change soil
+    case y:
+        //change soil
+    case z:
+        //change soil
+}
+}
+
+//Variable states
+//
+//
+//
