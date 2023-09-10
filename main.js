4d94e6140c6701e1a9b5e9c38aa07bccbf3aca64
lmnitzsche <logannitzsche1@gmail.com>
2023-09-10 11:03:44 -0500

    finished dropdown menus for climate and management, made gui improvements

--- main.js
+++ main.js
@@ -1,68 +1,144 @@
+const skyColor = document.querySelector(".sky");
 const soilColor = document.querySelector(".soil");
-const selectElement = document.getElementById("soilmenu");
+const selectElementSoil = document.getElementById("soilmenu");
+const selectElementClimate = document.getElementById("climatemenu");
+const selectElementManagement = document.getElementById("managementmenu");
+const selectElementCrop = document.getElementById("cropmenu");
 
-//OCP, pH, and TEB are calculated using the median of each soil type
+
+//OCP, pH, TEB, and texture (1-10) are calculated using the median of each soil type
 var OCP; 
 var pH; 
 var TEB; 
+var texture;
 
 var climate; 
-var cropType;
 var management; 
+var cropType;
 
-selectElement.addEventListener("change", function () {
-    const selectedOption = selectElement.value;
+//weights are calculated using the median of each crop type
+var textureWeight;
+var OCWeight;
+var TEBWeight;
+var pHWeight;
+var climateWeight;
+var cropTypeWeight;
+var managementWeight;
 
-    switch (selectedOption) {
+selectElementSoil.addEventListener("change", function () {
+    const selectedOptionSoil = selectElementSoil.value;
+
+    switch (selectedOptionSoil) {
         case "clay":
             soilColor.style.backgroundColor = "brown";
             pH = 7;
             OCP = 2;
             TEB = 25;
+            texture = 8;
             break;
         case "sand":
             soilColor.style.backgroundColor = "yellow";
             pH = 6.5;
-            OCP = .2; //fix
+            OCP = .2; 
             TEB = 12.5;
+            texture = 2;
             break;
         case "silty":
             soilColor.style.backgroundColor = "gray";
             pH = 7;
             OCP = 2;
             TEB = 20;
+            texture = 5;
             break;
         case "peaty":
             soilColor.style.backgroundColor = "darkolivegreen";
             pH = 4.5;
             OCP = 5;
             TEB = 7;
+            texture = 6;
             break;
         case "chalky":
             soilColor.style.backgroundColor = "white";
             pH = 8;
             OCP = .3; 
             TEB = 35;
+            texture = 7;
             break;
         case "loamy":
             soilColor.style.backgroundColor = "darkgoldenrod";
             pH = 6.75;
             OCP = 1.5;
             TEB = 20;
+            texture = 4;
             break;
         default:
-            soilColor.style.backgroundColor = "brown"; // Default color
+            soilColor.style.backgroundColor = "brown"; //clay
+            pH = 7;
+            OCP = 2;
+            TEB = 25;
+            texture = 8;
     }
 });
 
-function f(texture, OCP, TEB, pH) 
-{
-    // Coefficients (these are arbitrary, actual values would depend on the specific crop)
-    const textureWeight = 0.2;
-    const OCWeight = 0.3;
-    const TEBWeight = 0.1;
-    const pHWeight = 0.4;
+selectElementClimate.addEventListener("change", function () {
+    const selectedOptionClimate = selectElementClimate.value;
 
+    switch (selectedOptionClimate) {
+        case "arid":
+            skyColor.style.backgroundColor = "orange";
+            climate = 1;
+            break;
+        case "temperate":
+            skyColor.style.backgroundColor = "lightblue";
+            climate = 2;
+            break;
+        case "tropical":
+            skyColor.style.backgroundColor = "deepskyblue";
+            climate = 3;
+            break;
+        case "continental":
+            skyColor.style.backgroundColor = "darkblue";
+            climate = 4;
+            break;
+        case "mediterranean":
+            skyColor.style.backgroundColor = "lightgreen";
+            climate = 5;
+            break;
+        default:
+            skyColor.style.backgroundColor = "orange"; //arid
+            climate = 1;
+    }
+});
+
+selectElementManagement.addEventListener("change", function () {
+    const selectedOptionManagement = selectElementManagement.value;
+
+    switch (selectedOptionManagement) {
+        case "bad":
+            management = 1;
+            break;
+        case "sustainable":
+            management = 2;
+            break;
+        case "average":
+            management = 3;
+            break;
+        case "good":
+            management = 4;
+            break;
+        case "great":
+            management = 5;
+            break;
+        default:
+            management = 1;
+    }
+});
+
+
+
+
+function f(texture, OCP, TEB, pH, textureWeight, OCWeight, TEBWeight, pHWeight)
+{
     // Calculate the effect of each soil characteristic
     const textureEffect = textureWeight * texture;
     const OCEffect = OCWeight * OCP;
@@ -74,12 +150,8 @@ function f(texture, OCP, TEB, pH)
     return cropPerformance;
 }
 
-function g(climate, cropType, management) 
+function g(climate, cropType, management, climateWeight, cropTypeWeight, managementWeight) 
 {
-    // Coefficients (these are arbitrary, actual values would depend on the specific crop)
-    const climateWeight = 0.5;
-    const cropTypeWeight = 0.3;
-    const managementWeight = 0.2;
 
     // Calculate the effect of each factor
     const climateEffect = climateWeight * climate;
@@ -90,3 +162,17 @@ function g(climate, cropType, management)
     const cropPerformance = climateEffect + cropTypeEffect + managementEffect; 
     return cropPerformance;
 }
+
+/*function calculateSuccess(texture, OCP, TEB, pH, climate, cropType, management) {
+    // Calculate the effects using functions f and g
+    const soilEffect = f(texture, OCP, TEB, pH);
+    const environmentEffect = g(climate, cropType, management);
+
+    // Combine the effects using a weighted average
+    const overallPerformance = (soilEffect + environmentEffect) / 2;
+
+    // Convert the overall performance to a success percentage (you can adjust the scaling)
+    const successPercentage = (overallPerformance + 1) * 50; // Assuming overall performance ranges from -1 to 1
+
+    return successPercentage;
+}*/
